9bd5f6fbaa74ef0166481d896c7b253322ede57a
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-07-21 20:47:44 -0700

    Draw Extra Card

---
+++ Nonstick Summer 2025/Assets/Scripts/Cards/Stamps/DrawExtraCardStamp.cs
@@ -0,0 +1,24 @@
+/*****************************************************************************
+* File Name :         DrawExtraCardStamp.cs
+* Author :            Toby
+* Creation Date :     June 16, 2025
+*
+* Brief Description : 
+*****************************************************************************/
+
+using UnityEngine;
+
+public class DrawExtraCardStamp : ModifierStamp
+{
+    [SerializeField, Min(1)] private int NumCardsToDraw = 1;
+    protected override void EffectTriggered(CardData affectedCard)
+    {
+        if (DialogueUIController.Instance != null)
+        {
+            for (int i = 0; i < NumCardsToDraw; i++)
+            {
+                DialogueUIController.Instance.DeckDisplay.DrawOneCard();
+            }
+        }
+    }
+}

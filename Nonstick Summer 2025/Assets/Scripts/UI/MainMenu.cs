0a67254b867e5491d4e65a301a63d4522eeb9bea
Karkaranos <karkaranosyt@gmail.com>
2025-07-31 17:38:13 -0500

    Credits

--- Nonstick Summer 2025/Assets/Scripts/UI/MainMenu.cs
+++ Nonstick Summer 2025/Assets/Scripts/UI/MainMenu.cs
@@ -11,18 +11,30 @@
 
 using UnityEngine;
 using NaughtyAttributes;
-
+using System.Collections;
 public class MainMenu : MonoBehaviour
 {
     [Scene] [SerializeField] private int MainGameplayScene=1;
     [SerializeField] private GameObject controls;
-    [SerializeField] private GameObject credits;
+    [SerializeField] private GameObject creditParent;
+    [SerializeField] private GameObject creditScroll;
+    [SerializeField] private GameObject mainMenu;
+
+    [Header("Credit Controls")]
+    [SerializeField] private float creditSpeed;
+    [SerializeField] private float heightToReach;
+    [SerializeField] private float pauseBeforeStartEnd = 1.5f;
+
 
     private GameObject openMenu;
+    [SerializeField, ReadOnly] Vector3 creditStart;
+    private Coroutine credits;
 
     //maybe put cursor shenanigans here
     private void Start()
     {
+        creditStart = creditScroll.transform.localPosition;
+
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
 
@@ -47,8 +59,37 @@ public class MainMenu : MonoBehaviour
 
     public void OpenCredits()
     {
-        CloseMenu();
-        openMenu = Instantiate(credits);
+        mainMenu.SetActive(false);
+        creditParent.SetActive(true);
+        credits = StartCoroutine(ScrollCredits());
+    }
+
+    private void ResetCredits()
+    {
+        StopCoroutine(credits);
+        creditScroll.transform.localPosition = creditStart;
+    }
+
+    private IEnumerator ScrollCredits()
+    {
+        yield return new WaitForSeconds(pauseBeforeStartEnd);
+        Vector3 pos = creditScroll.transform.position;
+        while(pos.y < heightToReach)
+        {
+            yield return null;
+            pos.y += creditSpeed * Time.deltaTime * Mathf.Clamp(Screen.height/1280, 1, 3);
+            creditScroll.transform.position = pos;
+        }
+        yield return new WaitForSeconds(pauseBeforeStartEnd*2);
+        CloseCredits();
+
+    }
+
+    public void CloseCredits()
+    {
+        creditParent.SetActive(false);
+        mainMenu.SetActive(true);
+        ResetCredits();
     }
 
     public void CloseMenu()

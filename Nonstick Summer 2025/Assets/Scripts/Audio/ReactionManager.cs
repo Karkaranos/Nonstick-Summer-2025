c6c1ce501b64c1ffa6d32f82e863733b29839fc3
Joseph Consoli <114947767+JRavioli@users.noreply.github.com>
2025-08-04 16:08:04 -0500

    pre-meeting changes

--- Nonstick Summer 2025/Assets/Scripts/Audio/ReactionManager.cs
+++ Nonstick Summer 2025/Assets/Scripts/Audio/ReactionManager.cs
@@ -9,11 +9,17 @@ public class ReactionManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CharacterReactions = AudioManager.instance.CreateEventInstance(FMODEvents.instance.CharacterReactGenericSFX);
+        //TODO
+        //Have a switch statement here that sets CharacterReactions to the correct designated character rection FMODEvent
+        CharacterReactions = AudioManager.instance.CreateEventInstance(FMODEvents.instance.UncleReact);
     }
 
     public void PlayReaction(int val)
     {
+        //0 = neutral
+        //1 = happy
+        //2 = sad
+        //3 = angry
         CharacterReactions.setParameterByName("Reactions", val);
         CharacterReactions.start();
     }

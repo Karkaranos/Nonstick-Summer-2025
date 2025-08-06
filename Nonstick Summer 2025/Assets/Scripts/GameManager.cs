c2e02228a14a62eb8ff76a1d909b7c4704ef3a83
Karkaranos <karkaranosyt@gmail.com>
2025-08-06 18:08:02 -0500

    Cards reset after going to main menu

--- Nonstick Summer 2025/Assets/Scripts/GameManager.cs
+++ Nonstick Summer 2025/Assets/Scripts/GameManager.cs
@@ -102,8 +102,16 @@ public class GameManager : Singleton<GameManager>
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-
         DialogueManager.CurrentEnergy = _defaultEnergy;
+        if(scene.name.Contains("1"))
+        {
+            if(DeckManagerReference!=null)
+                DeckManagerReference = null;
+            if (RelationshipManagerReference != null)
+                RelationshipManagerReference = null;
+            DeckManagerReference = new DeckManager(startingCards);
+            RelationshipManagerReference = new RelationshipManager(grandmaStartingValue, uncleStartingValue, cousinStartingValue, momStartingValue);
+        }
 
     }
 

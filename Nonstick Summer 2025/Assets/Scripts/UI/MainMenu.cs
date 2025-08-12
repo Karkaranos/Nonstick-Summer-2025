5ca1e38b16830451b358320c6fe70114e5ce746e
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-08-12 13:28:35 -0500

    yaaaay invisible box

--- Nonstick Summer 2025/Assets/Scripts/UI/MainMenu.cs
+++ Nonstick Summer 2025/Assets/Scripts/UI/MainMenu.cs
@@ -38,7 +38,7 @@ public class MainMenu : MonoBehaviour
     [Tooltip ("Fade to black prefab in scene")]
     [SerializeField][Required] private GameObject fadeToBlack;
     [SerializeField][Required] private Image creditsFadeToBlack;
-    [SerializeField][Required] private Image creditsEndingFrame;
+    [SerializeField][Required] private RectTransform creditsEndingFrame;
     
 
     //maybe put cursor shenanigans here
@@ -90,14 +90,16 @@ public class MainMenu : MonoBehaviour
 
         yield return new WaitForSeconds(pauseBeforeStartEnd);
         Vector3 pos = creditScroll.transform.position;
-        while(creditsEndingFrame.transform.position.y < 0)
+        Debug.Log(creditsEndingFrame.position);
+        while (creditsEndingFrame.position.y < Screen.height/2)
         {
+            Debug.Log(creditsEndingFrame.position);
             // old input system lol
-            float speedUp = (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Space)) ? 4 : 1;
+            float speedUp = (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Space)) ? 8 : 1;
 
-            yield return null;
             pos.y += creditSpeed * Time.deltaTime * Mathf.Clamp(Screen.height/1280, 1, 3) * speedUp;
             creditScroll.transform.position = pos;
+            yield return null;
         }
         yield return new WaitForSeconds(pauseBeforeStartEnd);
 

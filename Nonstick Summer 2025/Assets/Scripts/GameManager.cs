683c8897262476f130f28ae4a75fdfd8424e561f
Karkaranos <karkaranosyt@gmail.com>
2025-08-13 12:04:40 -0500

    attempt at fixing continued relationships

--- Nonstick Summer 2025/Assets/Scripts/GameManager.cs
+++ Nonstick Summer 2025/Assets/Scripts/GameManager.cs
@@ -75,6 +75,7 @@ public class GameManager : Singleton<GameManager>
     {
         DontDestroyOnLoad(transform.parent == null ? this.gameObject : transform.parent.gameObject);
 
+
         UITransitionManagerReference = UITransitionManagerReference ?? new UITransitionManager();
         CardStyleManagerReference = CardStyleManagerReference ?? new CardStyleManager(Card_CharmingStyle, Card_AssertiveStyle, Card_SappyStyle,
             Card_StatementStyle, Card_QuestionStyle, BlankCard, YellowCardBack, RedCardBack, BlueCardBack);
@@ -107,6 +108,9 @@ public class GameManager : Singleton<GameManager>
         DialogueManager.CurrentEnergy = _defaultEnergy;
         if (scene.name.Contains("1"))
         {
+            DeckManagerReference = null;
+            RelationshipManagerReference = null;
+            ModifierManagerReference = null;
             DeckManagerReference = new DeckManager(startingCards);
             RelationshipManagerReference = new RelationshipManager(grandmaStartingValue, uncleStartingValue, cousinStartingValue, momStartingValue);
             ModifierManagerReference = new ModifierManager(startingModifiers);
